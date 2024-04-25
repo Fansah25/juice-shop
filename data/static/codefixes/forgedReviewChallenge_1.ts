ec9f23e4183f867e5179d74f266d2f1995bcc9e2
Faith Ansah <fansah@drew.edu>
2024-04-24 22:51:55 -0400

    fix it challenge code from Forged Review

--- data/static/codefixes/forgedReviewChallenge_1.ts
+++ data/static/codefixes/forgedReviewChallenge_1.ts
@@ -2,7 +2,7 @@ module.exports = function productReviews () {
   return (req: Request, res: Response, next: NextFunction) => {
     const user = security.authenticatedUsers.from(req)
     db.reviews.update(
-      { _id: req.body.id },
+      { _id: req.body.id, author: user.data.email }, // correct code from the fix it challenge
       { $set: { message: req.body.message, author: user.data.email } },
       { multi: true }
     ).then(
